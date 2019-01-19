6db168719032009697e6544e35011d9287248e9f
atranarta <radyuk.tatyana@gmail.com>
2019-01-19 19:25:45 +0100

    Created brain-prime, updated version 1.0.9

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ install: install-deps
 install-deps:
 	npm install
 	
-start: start-brain-progression
+start: start-brain-prime
 
 start-brain-calc:
 	npx babel-node -- src/bin/brain-calc.js
@@ -15,7 +15,10 @@ start-brain-games:
 	npx babel-node -- src/bin/brain-games.js
 
 start-brain-gcd:
-	npx babel-node -- src/bin/brain-gcd.js	
+	npx babel-node -- src/bin/brain-gcd.js
+
+start-brain-prime:
+	npx babel-node -- src/bin/brain-prime.js	
 
 start-brain-progression:
 	npx babel-node -- src/bin/brain-progression.js		
---
+++ src/games/brain-prime.js
@@ -0,0 +1,34 @@
+import { gameEngine } from '..';
+import { cons } from 'hexlet-pairs';
+import getRandomNumber from '../utils';
+
+const isPrime = (number) => {
+  if (number === 1) {
+    return false;
+  }
+  for (let divider = 2; divider * divider <= number; divider += 1) {
+    if (number % divider === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const getCorrectAnswer = number => (isPrime(number) ? 'yes' : 'no');
+
+const qustionAnswerCreator = () => {
+  const question = getRandomNumber(1, 100);
+  const answer = getCorrectAnswer(question);
+
+  return cons(question, answer);
+};
+
+const description = 'Answer "yes" if number even otherwise answer "no".';
+
+export default () => {
+  gameEngine(
+    description,
+    qustionAnswerCreator,
+  );
+};
